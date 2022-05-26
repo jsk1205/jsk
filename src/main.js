58b69e3e363a2a22c7ce00be1050269568115cc3
焦水笛 <jiaoshuidi@dataocean.com.cn>
2022-05-26 17:12:24 +0800

    vue-demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+// import router from './router'
+import store from './store'
+// import Axios from 'axios';
+// import {proxy, unProxy} from "ajax-hook"; 
+Vue.config.productionTip = false;
+Vue.prototype.$store=store;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/mock/index.js
@@ -0,0 +1,5 @@
+var Mock=require('mockjs');
+//定义数据模型
+var data=Mock.mock({
+    
+})
